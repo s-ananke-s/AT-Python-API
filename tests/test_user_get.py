798cd7ec8b6c5c9bf5fb23fc5a623ff62b3902d6
Lana <s.ananke.s@gmail.com>
2021-12-06 17:05:43 +0300

    Ex18 - добавлены allure марки в test_user_get.py

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -1,46 +1,63 @@
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import allure
+
 
 class TestUserGet(BaseCase):
+    @allure.feature("Get user")
+    @allure.story("Поулчение данных пользователя неавторизованным пользователем")
     def test_get_user_details_not_auth(self):
-        response = MyRequests.get("/user/2")
-        Assertions.assert_json_has_key(response, "username")
-        Assertions.assert_json_has_not_key(response, "email")
-        Assertions.assert_json_has_not_key(response, "firstName")
-        Assertions.assert_json_has_not_key(response, "lastName")
+        with allure.step("Пытаемся получить данные пользователя с id 2"):
+            response = MyRequests.get("/user/2")
+        with allure.step("Проверяем, что доступен username"):
+            Assertions.assert_json_has_key(response, "username")
+        with allure.step("Проверяем, что не доступен email"):
+            Assertions.assert_json_has_not_key(response, "email")
+        with allure.step("Проверяем, что не доступен firstName"):
+            Assertions.assert_json_has_not_key(response, "firstName")
+        with allure.step("Проверяем, что не доступен lastName"):
+            Assertions.assert_json_has_not_key(response, "lastName")
 
+    @allure.story("Получение данных пользователя авторизованным пользователем")
     def test_get_user_details_auth_as_same_user(self):
-        data = {
-            'email': 'vinkotov@example.com',
-            'password': '1234'
-        }
+        with allure.step("Заходим под пользователем с id 2"):
+            data = {
+                'email': 'vinkotov@example.com',
+                'password': '1234'
+            }
+            response1 = MyRequests.post("/user/login", data=data)
+        with allure.step("Запоминаем данные этого пользователя"):
+            auth_sid = self.get_cookie(response1, "auth_sid")
+            token = self.get_header(response1, "x-csrf-token")
+            user_id_from_auth_method = self.get_json_value(response1, "user_id")
+        with allure.step("Пытаемся получить данные пользователя с id 2"):
+            response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
+                                       headers = {"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
+        with allure.step("Проверяем, что все поля доступны"):
+            expected_fields = ["username", "email", "firstName", "lastName"]
+            Assertions.assert_json_has_keys(response2, expected_fields)
 
-        response1 = MyRequests.post("/user/login", data=data)
-
-        auth_sid = self.get_cookie(response1, "auth_sid")
-        token = self.get_header(response1, "x-csrf-token")
-        user_id_from_auth_method = self.get_json_value(response1, "user_id")
-
-        response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
-                                   headers = {"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
-
-        expected_fields = ["username", "email", "firstName", "lastName"]
-        Assertions.assert_json_has_keys(response2, expected_fields)
-
+    @allure.story("Получение данных пользователя другим авторизованным пользователем")
     def test_get_other_user(self):
-        data = {
-            'email': 'vinkotov@example.com',
-            'password': '1234'
-        }
-
-        response1 = MyRequests.post("/user/login", data=data)
-        auth_sid = self.get_cookie(response1, "auth_sid")
-        token = self.get_header(response1, "x-csrf-token")
-        user_id_from_auth_method = self.get_json_value(response1, "user_id")
-        print(user_id_from_auth_method)
-        response2 = MyRequests.get("/user/1", headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
-        Assertions.assert_json_has_key(response2, "username")
-        Assertions.assert_json_has_not_key(response2, "email")
-        Assertions.assert_json_has_not_key(response2, "firstName")
-        Assertions.assert_json_has_not_key(response2, "lastName")
+        with allure.step("Заходим под пользователем с id 2"):
+            data = {
+                'email': 'vinkotov@example.com',
+                'password': '1234'
+            }
+            response1 = MyRequests.post("/user/login", data=data)
+        with allure.step("Запоминаем данные этого пользователя"):
+            auth_sid = self.get_cookie(response1, "auth_sid")
+            token = self.get_header(response1, "x-csrf-token")
+            user_id_from_auth_method = self.get_json_value(response1, "user_id")
+            print(user_id_from_auth_method)
+        with allure.step("Пытаемся получить данные другого пользователя с id 1"):
+            response2 = MyRequests.get("/user/1", headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
+        with allure.step("Проверяем, что доступен username"):
+            Assertions.assert_json_has_key(response2, "username")
+        with allure.step("Проверяем, что не доступен email"):
+            Assertions.assert_json_has_not_key(response2, "email")
+        with allure.step("Проверяем, что не доступен firstName"):
+            Assertions.assert_json_has_not_key(response2, "firstName")
+        with allure.step("Проверяем, что не доступен lastName"):
+            Assertions.assert_json_has_not_key(response2, "lastName")
